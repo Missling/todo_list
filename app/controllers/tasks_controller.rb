d5b6793911ea5c0c474ef03608c884f2729e0411
missling <ling.giang@gmail.com>
2015-10-20 17:28:57 -0700

    implemented sms using twilio

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,9 +1,37 @@
-# require 'date'
-
 class TasksController < ApplicationController
+  skip_before_filter  :verify_authenticity_token, only: [:twilio_action]
+
   def index
     @task = Task.new
     @tasks = Task.order('completed, priority DESC')
+  end
+
+  def twilio_action
+    message = params[:Body].split(",")
+    description = message.first
+    priority = message.last
+
+    if description == 'Show'
+      tasks = Task.where(completed: false)
+
+      if tasks.any?
+        list = tasks.map(&:description)
+        render xml: twilio_message(list.join(", "))
+      else
+        render xml: twilio_message("Todo list is empty") 
+      end
+    else
+      priority = 1 if !(1..5).include?(priority)
+
+      task = Task.new(
+        description: description,
+        priority: priority
+      )
+
+      if task.save
+        render xml: twilio_message("#{description} has been added to your todo list" )  
+      end
+    end
   end
 
   def create
@@ -38,5 +66,12 @@ class TasksController < ApplicationController
 
   def task_params
     params.require(:task).permit(:description, :priority)
+  end
+
+  def twilio_message(message)
+    "<?xml version=\"1.0\" encoding=\"UTF-8\" ?>
+      <Response>
+        <Message>#{message}</Message>
+      </Response>"
   end
 end
